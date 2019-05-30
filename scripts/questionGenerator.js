fd69549ee0f93c6300ee0876636c8e9a4890d5c5
Markian Tsybran <mr.tsybran@gmail.com>
2019-05-30 23:11:00 +0300

    quiz mocks + funcs; v1

--- scripts/questionGenerator.js
+++ scripts/questionGenerator.js
@@ -3,13 +3,16 @@ var QuestionGenerator = {
   },
   sessionId: null,
   startQuiz: function(){
+    $('body').addClass('cloak');
+
     $.ajax({
-        url: apiUrl + '/spender_quiz_submissions',
-        method: 'post'
-      }).done(function(res){
-        QuestionGenerator.sessionId = res.session_id
-        QuestionGenerator.populateQuestionAndProgress(res.question, res.progress, res.percent_complete)
-      })
+      url: apiUrl + '/spender_quiz_submissions',
+      method: 'post'
+    }).done(function(res){
+      QuestionGenerator.sessionId = res.session_id;
+      QuestionGenerator.populateQuestionAndProgress(res.question, res.progress, res.percent_complete);
+      changePageAnim('quiz_active');
+    })
   },
   submitQuestion: function(questionName, answer){
     $.ajax({
@@ -18,27 +21,45 @@ var QuestionGenerator = {
       data: {
         session_id: QuestionGenerator.sessionId,
         response: {
-            question: questionName,
-            answer: answer
+          question: questionName,
+          answer: answer
         }
       }
     }).done(function(res){
-      QuestionGenerator.sessionId = res.session_id
+      QuestionGenerator.sessionId = res.session_id;
       if (res.type == 'question'){
-        QuestionGenerator.populateQuestionAndProgress(res.question, res.progress, res.percent_complete)
+        QuestionGenerator.populateQuestionAndProgress(res.question, res.progress, res.percent_complete);
       } else if (res.type == 'result') {
-        QuestionGenerator.populatesResult(res.result)
+        $('body').addClass('cloak');
+        QuestionGenerator.populatesResult(res.result);
       }
     })
   },
   populateQuestionAndProgress: function(question, progress, percent_complete){
-    // Populate a new question screen
-    console.log(progress)
-    console.log(percent_complete)
-    console.log(question)
+    var choices_html = '';
+
+    if($('#progress').length){
+      $('#progress').width(100*percent_complete + '%');
+      $('#progressNum').text(progress);
+    }
+
+    $('#quiz_question').text(question.body);
+
+    question.choices.map(function(choice){
+      choices_html += '<div class="storeBlock" data-name="' + choice.name + '">' +
+        '<img src="./images/' + choice.image + '.png" alt="' + choice.name + '"/>' +
+        '<p>' + choice.body + '</p></div>';
+    });
+
+    $('#quiz_choices').html(choices_html);
+
+    $('.storeBlock').click(function(){
+      QuestionGenerator.submitQuestion(question.name, this.dataset.name);
+    });
   },
   populatesResult: function(result){
-    // Populate result screen
-    console.log(result)
+    $('body').removeClass('quiz_active');
+    $('#resultTitle').text(result);
+    changePageAnim('result_active');
   }
-}
+};
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
-apiUrl = 'https://clasp-subs.herokuapp.com'
+const apiUrl = 'https://clasp-subs.herokuapp.com';
+
+const changePageAnim = function (class_name) {
+  setTimeout(function(){
+    $('body').addClass(class_name).removeClass('cloak');
+  }, 250);
+};
+
 $(document).ready(function(){
-})
+  $('#startQuiz').click(QuestionGenerator.startQuiz);
+});
