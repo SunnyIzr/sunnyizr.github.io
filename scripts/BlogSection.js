f45a5b2aac1a5ec68fc32d61e473557c88aab4a7
Markian Tsybran <mr.tsybran@gmail.com>
2019-10-31 17:28:49 +0200

    add savings calculator screens

---
+++ scripts/BlogSection.js
@@ -0,0 +1,95 @@
+var BlogSection = {
+  init: function(){
+    if( ($('#article-page').length > 0) ||  ($('.home-template').length > 0)){
+      this.togglePopupListener();
+      this.emailSubmitPopupEventListener();
+      this.emailSubmitBlockEventListener()
+    }
+  },
+  emailSubmitPopupEventListener: function(){
+    $('#blogEmailSignup').on('submit', function(event){
+      event.preventDefault()
+      var email = $(this).find('input').val()
+      BlogSection.submitEmail(email, true)
+    })
+  },
+  emailSubmitBlockEventListener: function(){
+    $('#bottomBlogEmailSignup').on('submit', function(event){
+      event.preventDefault()
+      var email = $(this).find('input').val()
+      BlogSection.submitEmail(email, false)
+    })
+  },
+  submitEmail: function(email, popup){
+    if (popup == true){
+      UserSignUps.submitData(null, null, email, 'Blog Email Sign Up', 'Pop Up', false, null, BlogSection.renderPopupSuccess, BlogSection.renderPopupError)
+    } else {
+      UserSignUps.submitData(null, null, email, 'Blog Email Sign Up', 'Bottom Block', false, null, BlogSection.renderBlockSuccess, BlogSection.renderBlockError)
+    }
+  },
+  togglePopupListener: function(){
+    // if (true){
+    if (!BlogSection.getCookie('claspblogemail')){
+      var timeout = setTimeout(function(){
+        BlogSection.togglePopup();
+        clearTimeout(timeout);
+        timeout = null;
+        BlogSection.setCookie()
+      }, 15000);
+      $('.closeImg.togglePopup').on('click', BlogSection.togglePopup);
+    }
+  }, 
+  togglePopup: function(){
+    if($('#article-page').length > 0){
+      if ( $('body').hasClass('activePopup') ){
+        mixpanel.track('Blog Popup Off', {});
+      } else {
+        mixpanel.track('Blog Popup On', {});
+      }
+      $('body').toggleClass('activePopup');
+      if ( $(window).width() > 500 ){
+        $('.activePopup #blogEmailSignup input[type=email]').focus()
+      }
+    }
+  },
+  renderPopupSuccess: function(){
+    $('.popupHolder').addClass('submit-success')
+  },
+  renderPopupError: function(){
+    $('.popupHolder').addClass('submit-error')
+  },
+  renderBlockSuccess: function(){
+    $(".afterSignUp").addClass("active");
+  },
+  renderBlockError: function(){
+    $('.signUpBlock').addClass('submit-error')
+  },
+  setCookie: function(){
+    var now = new Date();
+    var time = now.getTime();
+    time += 14 * 24 * 60 * 60 * 1000;
+    now.setTime(time);
+    console.log("claspblogemail=true; " + now.toUTCString() + ";")
+    document.cookie = "claspblogemail=true; expires=" + now.toUTCString() + "; path=/"
+  },
+  getCookie: function(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+  },
+  trackSocialShare: function(){
+    $('#article-page .social-shares a').on('click', function(e){
+      var platform = $(this).data('platform')
+
+      mixpanel.track('Blog Social Share', {'platform': platform});
+
+    })
+  }
+}
+
+
---
+++ scripts/EmotionalSpenderQuiz.js
@@ -0,0 +1,163 @@
+var stopTime;
+
+var EmotionalSpenderQuiz = {
+  init: function(){
+    this.startQuizListener()
+    this.loadLeadGenCopyListener()
+    this.submitEmailAndNameListener()
+    this.trackSocialShare()
+  },
+  sessionId: null,
+  result: null,
+  startQuizListener: function(){
+    $('#startQuiz, #startQuiz2').click(EmotionalSpenderQuiz.startQuiz);
+  },
+  loadLeadGenCopyListener: function(){
+    $('#loadLeadGen').click(function(){
+      mixpanel.track('Emotional Spender Quiz Show More Content');
+      $('.leadGenContent').show();
+
+      $('.social_sp_block').animate({
+          scrollTop: $(this)[0].scrollHeight
+      }, 700);
+
+      setTimeout (function(){
+        $('.mainQuiz').animate({
+          scrollTop: $("#leadGenContent").offset().top + 330},
+        'slow');
+      }, 300);
+
+      setTimeout(function(){
+        $('.leadGenContent').addClass('active');
+
+        var result = $('#resultTitle').text();
+        $('#resultSpan').html('So <span class="bold_text">' + result.toLowerCase() + '</span>, huh?');
+        $('.start_here-button-holder').removeClass('active');
+      }, 150)
+    })
+  },
+  submitEmailAndNameListener: function(){
+    $('#spenderQuizEmailSubmission').on('submit', function(event){
+      event.preventDefault()
+      var email = $("#emailOfSpender").val();
+      var phone = "1" + $("#PhoneOfSpender").cleanVal();
+      var name = $("#NameOfSpender").val();
+
+      function successFunc(){
+        $(".spender-email-block" ).addClass("validation-complete");
+        var result = EmotionalSpenderQuiz.result
+        mixpanel.people.set({ "Emotional Spender Result": result});
+        mixpanel.track('Emotional Spender Show Result');
+        EmotionalSpenderQuiz.loadShareASalePixel()
+      }
+
+      function errorFunc(){
+        $(".email-block-holder" ).addClass("error-active");  
+      }
+      if (validateEmail(email) && phone.length && !isNaN(phone) && name.length) {
+        UserSignUps.submitData(name, phone, email, 'Emotional Spender Quiz Email Sign Up', null, false, EmotionalSpenderQuiz.sessionId, successFunc, errorFunc )
+      } else {
+        errorFunc()
+      }
+    })
+  },
+  startQuiz: function(){
+    mixpanel.track('Emotional Spender Quiz Start');
+
+    $('body').addClass('cloak');
+
+    $.ajax({
+      url: apiUrl + '/spender_quiz_submissions',
+      method: 'post'
+    }).done(function(res){
+      EmotionalSpenderQuiz.sessionId = res.session_id;
+      EmotionalSpenderQuiz.populateQuestionAndProgress(res.question, res.progress, res.percent_complete);
+      changePageAnim('quiz_active');
+    })
+  },
+  submitQuestion: function(questionName, answer){
+    mixpanel.track('Emotional Spender Quiz Submit Question', {'questionName': questionName, 'answer': answer});
+    $.ajax({
+      url: apiUrl + '/spender_quiz_submissions',
+      method: 'patch',
+      data: {
+        session_id: EmotionalSpenderQuiz.sessionId,
+        response: {
+          question: questionName,
+          answer: answer
+        }
+      }
+    }).done(function(res){
+      EmotionalSpenderQuiz.sessionId = res.session_id;
+
+      if(stopTime) clearTimeout(stopTime);
+      $('#quiz_question').addClass("animText");
+
+      stopTime = setTimeout(function () {
+        if (res.type === 'question'){
+            EmotionalSpenderQuiz.populateQuestionAndProgress(res.question, res.progress, res.percent_complete);
+            $('#quiz_choices').removeClass("active");
+            $('#quiz_question').removeClass("animText");
+        } else if (res.type === 'result') {
+          $('body').addClass('cloak');
+          EmotionalSpenderQuiz.populatesResult(res.result);
+        }
+    },2000); 
+    })
+  },
+  populateQuestionAndProgress: function(question, progress, percent_complete){
+    var choices_html = '';
+
+    if($('#progress').length){
+      $('#progress').width(100*percent_complete + '%');
+      $('#progressNum').text(progress);
+    }
+
+    $('#quiz_question').text(question.body);
+
+    question.choices.map(function(choice){
+      choices_html += '<div class="storeBlock" data-name="' + choice.name + '">' +
+        '<img src="./images/' + choice.image + '.png" alt="' + choice.name + '"/>' +
+        '<p>' + choice.body + '</p></div>';
+    });
+
+    $('#quiz_choices').html(choices_html);
+
+    $('.storeBlock').click(function(){
+      $(this).addClass("active");
+      $('#quiz_choices').addClass("active");  
+      EmotionalSpenderQuiz.submitQuestion(question.name, this.dataset.name);
+    });
+  },
+  populatesResult: function(result){
+    resultTitle = result.title.toLowerCase().replace("you're a ", "")
+    EmotionalSpenderQuiz.result = resultTitle
+    $('#resultTitle').text(result.title);
+    $('#resultImg').attr('src', './images/' + result.image + '.jpg');
+    $('#resultTips').html(result.tips);
+    $('#resultDesc').html(result.desc);
+    setTimeout(function(){
+      $('#NameOfSpender').focus()
+    }, 1000)
+
+    mixpanel.track('Emotional Spender Show Email Capture', {'result': result.title});
+
+    changePageAnim('result_active', 'quiz_active');
+  },
+  trackSocialShare: function(){
+    $('.social-shares a').on('click', function(e){
+      var platform = $(this).data('platform')
+
+      mixpanel.track('Emotional Spender Quiz Social Share', {'platform': platform});
+    })
+  },
+  loadShareASalePixel: function(){
+    var ssid = shareasaleGetCookie('shareasaleSSCID')
+    var lid = mixpanel.get_distinct_id()
+    $('body').append("<img src='https://www.shareasale.com/sale.cfm?tracking=" + lid + "&amount=0.00&merchantID=92256&transtype=lead&sscidmode=6&sscid=" + ssid + " width='1' height='1'>")
+    $.getScript("https://shareasale-analytics.com/j.js", function() {
+    });
+  }
+};
+
+function shareasaleGetCookie(e){var r=e+"=";var a=decodeURIComponent(document.cookie);var o=a.split(";");for(var n=0;n<o.length;n++){var t=o[n];while(t.charAt(0)==" "){t=t.substring(1)}if(t.indexOf(r)==0){return t.substring(r.length,t.length)}}return""}
